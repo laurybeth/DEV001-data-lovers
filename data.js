afb767a88b0545c59ff45adeac0a37397911802b
laurybeth <laurybeth@msn.com>
2022-11-13 23:42:23 -0500

    Updates

--- data.js
+++ data.js
@@ -35,56 +35,62 @@ export const filterDataByProperties = (data, properties) => {
 
 };
 
-export const sortDataAZ= (data,sortBy) => {
+export const sortDataAZ = (data, sortBy) => {
   return [...data].sort((itemLeft, itemRight) => {
-      // el titulo de la izquierda va primero
-      if (itemLeft[sortBy] < itemRight[sortBy]) {
-        return -1;
-      }
-      // el titulo de la derecha va primero
-      if (itemLeft[sortBy] > itemRight[sortBy]) {
+    // el titulo de la izquierda va primero
+    if (itemLeft[sortBy] < itemRight[sortBy]) {
+      return -1;
+    }
+    // el titulo de la derecha va primero
+    if (itemLeft[sortBy] > itemRight[sortBy]) {
 
-        return 1;
-      }
-      // los titulos son iguales
-      return 0;
+      return 1;
+    }
+    // los titulos son iguales
+    return 0;
   });
 
 };
 
-export const sortDataZA= (data,sortBy) => {
+export const sortDataZA = (data, sortBy) => {
   return [...data].sort((itemLeft, itemRight) => {
-      // el titulo de la izquierda va primero
-      if (itemLeft[sortBy] < itemRight[sortBy]) {
-        return 1;
-      }
-      // el titulo de la derecha va primero
-      if (itemLeft[sortBy] > itemRight[sortBy]) {
+    // el titulo de la izquierda va primero
+    if (itemLeft[sortBy] < itemRight[sortBy]) {
+      return 1;
+    }
+    // el titulo de la derecha va primero
+    if (itemLeft[sortBy] > itemRight[sortBy]) {
 
-        return -1;
-      }
-      // los titulos son iguales
-      return 0;
+      return -1;
+    }
+    // los titulos son iguales
+    return 0;
   });
 
 };
 
 
-export const averageFunction = (data, condition) => {
-/*  
- //parseInt[data[condition], 10].reduce (function (accumulator, currentValue) =>  ((accumulator + currentValue)));
- 
- let sum = 0;
- data.reduce(function(data, condition){
-  sum+=parseInt(data[condition],10);
-  return sum;
-});*/
+export const averageFunction = (data, condition) => {console.log("c:"+data[0][condition]);
 
-let sum = 0;
-  data.forEach(data => {
-    sum += parseInt(data[condition], 10);
-  });
-     
-return (sum/data.length).toFixed(2);
+   /*let sum = [...data].reduce(function (accumulator, currentValue) {
 
+    return {
+      
+      data[condition]: parseInt(accumulator[condition],10) + parseInt(currentValue[condition],10)
+    };
+  }, { condition: 0 });
+
+  console.log("sumaaaa:"+sum[condition]);
+
+  return (sum.condition / data.length).toFixed(2);
+
+  data.reduce (function (accumulator, currentValue) =>  ((accumulator + currentValue)));
+ */
+
+  let sum = 0;
+    data.forEach(data => {
+      sum += parseInt(data[condition], 10);
+    });
+       
+  return (sum/data.length).toFixed(2);
 };
